ecb443d218967bfb792f6127ceb7efd9e1a56a00
Matt Russo <matthew.russo91@gmail.com>
2025-06-20 20:43:55 -0400

    fetch mock cleanup data from json-server

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import CleanupFeedItem from './CleanupFeedItem'
 
 const Feed = () => {
+  const [cleanups, setCleanups] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCleanups = async () => {
+      // const apiUrl = '/api/cleanups';
+      const apiUrl = 'http://localhost:5000/cleanups/'
+      
+      try {
+        const res = await fetch(apiUrl);
+        const data = await res.json();
+        setCleanups(data);
+      } catch (error) {
+        console.log('Error fetching data', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    console.log(JSON.stringify(cleanups))
+    fetchCleanups();
+  }, []);
+
   return (
     <>
-      <CleanupFeedItem />
+      {loading ? (<h2>Loading...</h2>) : (
+        <CleanupFeedItem />
+      )}
     </>
   )
 }
